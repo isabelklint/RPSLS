73c60a3d59e0a30c21cb1d9af7f14e69c264f4fc
wasline <sf.wasline@gmail.com>
2022-10-03 20:47:58 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
 import "./App.css";
-import Game from "./components/Game.js"
-import Header from "./components/Header.js";
+import Game from "./components/Game.js";
+import Footer from "./components/Footer.js";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <h1>Rock Paper Scissors lizard Spock</h1>
-        <Header />
+        <header>
+          <h1>Rock Paper Scissors lizard Spock</h1>
+        </header>
         <Game />
+        <Footer />
       </div>
     </div>
   );
